52745557aaa32d23025a5b22151da571a646764d
Aditya Kumar <aditya220972@gmail.com>
2023-06-17 10:27:06 +0530

    Time: 957 ms (6.67%), Space: 337.7 MB (5.03%) - LeetHub

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  void treetoGr(TreeNode* root,map<int ,vector<int>>&adj)
+{
+  if(!root) return ;
+  int par=root->val;
+  if(root->left) {
+    int l=root->left->val;
+    adj[l].push_back(par);
+    adj[par].push_back(l);
+      treetoGr(root->left,adj);
+  }
+  
+  if(root->right) {
+    int r=root->right->val;
+    adj[r].push_back(par);
+    adj[par].push_back(r);
+      treetoGr(root->right,adj);
+  }
+}
+void bfs(map<int ,vector<int>>&adj,vector<int>&vis,int src,int &ans)
+{
+    queue<pair<int,int>>q;   //node //lvl
+    q.push({src,0});
+    while(q.size())
+    {
+        int tp=q.front().first;
+        int lv=q.front().second;
+        vis[tp]=1;
+        ans=max(ans,lv);
+        q.pop();
+        for(auto c:adj[tp])
+        {if(!vis[c]) q.push({c,lv+1});}
+    }
+}
+      int amountOfTime(TreeNode* root, int start) {
+        map<int,vector<int>>adj;
+        treetoGr(root,adj);
+        int ans=INT_FAST32_MIN;
+        vector<int>vis(1e5+1,0);
+        bfs(adj,vis,start,ans);
+        // deb(ans);
+    return ans;
+    }
+};

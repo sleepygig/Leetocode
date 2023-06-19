c7730cd76f34f2eacaca78502b3cf6a2ee699372
Aditya Kumar <aditya220972@gmail.com>
2023-06-19 05:39:10 +0530

    Time: 17 ms (42.97%), Space: 19.8 MB (88.76%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -14,28 +14,24 @@ public:
    bool isleaf(TreeNode* root) {
     return (root->left == NULL && root->right == NULL);
 } 
-    void fs(TreeNode* root, int targetSum ,vector<vector<int>>&ans,vector<int>v,int cur)
+    void fs(TreeNode* root, int targetSum ,vector<vector<int>>&ans,vector<int>&v,int cur)
     {
          if(!root) return ;
         cur+=root->val;
-        // deb(root->data);
-        // deb(cur);
         v.push_back(root->val);
         if( isleaf(root) and  targetSum==cur){ans.push_back(v);}
         
         if(root->left)fs(root->left,targetSum,ans,v,cur);
         if(root->right)fs(root->right,targetSum,ans,v,cur);
+        v.pop_back();
     }
 
   	   vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
           vector<vector<int>>ans;
           vector<int>q;
           int cur=0;
-        //   deb(cur);
           fs(root,targetSum,ans,q,cur);
           return ans;
-        //   deb(ans.size());
-        //   for(auto c:ans)
-        //   {for(auto x:c){cout<<x<<" ";}cout<<endl;}
+
     }
 };

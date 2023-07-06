6dc81f88820e1d4006eb4a4c5a5a241920d06504
Aditya Kumar <aditya220972@gmail.com>
2023-07-06 23:24:25 +0530

    Time: 52 ms (14.1%), Space: 28.5 MB (6.75%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
- int minSubArrayLen(int target, vector<int>& nums) {
-    int n = nums.size();
-    int i = 0;
-    int j = 0;
-    int sum = 0;
-    int ans = INT_MAX;
-
-    while (j < n) {
-        sum += nums[j];
-        if (sum < target) {
-            j++;
-        } 
-               else 
+   int minSubArrayLen(int target, vector<int>& nums)
+   {
+    int n=nums.size();
+    
+      int ans =INT32_MAX;
+     int psum[n+1];
+     psum[0]=nums[0];
+     for(int i=1;i<n;i++)
+     {
+        psum[i]=psum[i-1]+nums[i];
+     }
+     psum[n]=0;
+       int i=0;
+      int j=0;
+     while(j<n )
+     {
+      if(psum[j]-psum[i]+nums[i]<target)
+      {
+        j++;
+      }
+      else 
+      {
+        
+        while(psum[j]-psum[i]+nums[i]>=target)
         {
-          while(sum>=target)
-          {
-            if(sum>= target)ans=min(ans,j-i+1);
-            sum-=nums[i];
-            i++;
-          }
-          j++;
+          ans =min(ans,j-i+1);
+          i++;
         }
-    }
-    //  ans = min(j - i + 1, ans);
-
-    int a = (ans == INT_MAX) ? 0 : ans;
-    return a;
-}
-
+      }
+     }
+  int a=(ans==INT32_MAX)? 0: ans;
+  // deb(a);
+  return a;
+   }
 };

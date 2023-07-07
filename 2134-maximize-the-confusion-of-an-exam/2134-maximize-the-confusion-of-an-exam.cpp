3d0e33bdfd78c952fa550d9f108a89f7b39dc3e9
Aditya Kumar <aditya220972@gmail.com>
2023-07-07 13:13:31 +0530

    Time: 23 ms (88.45%), Space: 10.1 MB (59.24%) - LeetHub

---
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+     int maxConsecutiveAnswers(string answerKey, int k) {
+    int n = answerKey.size();
+    int maxConsecutive = 0;
+    int maxCount = 0;
+    int start = 0;
+    int countT = 0;
+    int countF = 0;
+
+    for (int end = 0; end < n; end++) {
+        if (answerKey[end] == 'T') {
+            countT++;
+        } else {
+            countF++;
+        }
+
+        // Check if the current window violates the condition
+        // (number of changes used up)
+        while (min(countT, countF) > k) {
+            if (answerKey[start] == 'T') {
+                countT--;
+            } else {
+                countF--;
+            }
+            start++;
+        }
+
+        maxConsecutive = max(maxConsecutive, end - start + 1);
+    }
+
+    return maxConsecutive;
+}
+
+};

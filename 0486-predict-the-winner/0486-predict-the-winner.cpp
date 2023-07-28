0553a73201b36461db914a6c834e046ee7fea637
Aditya Kumar <aditya220972@gmail.com>
2023-07-28 16:48:07 +0530

    Time: 113 ms (34.83%), Space: 7.3 MB (75.78%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-   int sc(vector<int>& nums, int i, int j) {
-    if (i == j) return nums[i];
-    if (i > j) return 0;
+   int sc(vector<int>&nums,int i,int j,int sum,bool &a){
+    {
+        if(i==j) return nums[i];
+        if(i>j) return 0;
+    }
+    int l=nums[i]+min(sc(nums,i+2,j,sum,a),sc(nums,i+1,j-1,sum,a));   
+    int r=nums[j]+min(sc(nums,i+1,j-1,sum,a),sc(nums,i,j-2,sum,a));
+ return max(l,r);
+      
+    
+    return 0;
+    }
+ bool PredictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        bool a=false;
+        if(n==1) return true;
+       int x= sc(nums,0,n-1,sum,a);
+        return x>=sum-x;
 
-    int left = nums[i] + min(sc(nums, i + 2, j), sc(nums, i + 1, j - 1));
-    int right = nums[j] + min(sc(nums, i + 1, j - 1), sc(nums, i, j - 2));
 
-    return max(left, right);
-}
-
-bool PredictTheWinner(vector<int>& nums) {
-    int n = nums.size();
-    if (n == 1) return true;
-
-    int player1Score = sc(nums, 0, n - 1);
-    int totalScore = accumulate(nums.begin(), nums.end(), 0);
-    int player2Score = totalScore - player1Score;
-
-    return player1Score >= player2Score;
-}
+    }
 
 };

63ae35291c38224204f6945e6add810eb233e9bd
Aditya Kumar <aditya220972@gmail.com>
2023-08-21 23:07:00 +0530

    Time: 407 ms (96.73%), Space: 104.7 MB (48.61%) - LeetHub

---
+++ 2118-maximum-earnings-from-taxi/2118-maximum-earnings-from-taxi.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+	long long f(int i,vector<vector<int>>& r, vector<int>& st,int m,vector<long long>& dp){
+		if(i>=m) return 0;
+		if(dp[i]!=-1) return dp[i];
+		int ind=lower_bound(st.begin(),st.end(),r[i][1])-st.begin();
+		long long pick=(r[i][1]-r[i][0]+r[i][2])+f(ind,r,st,m,dp);
+		long long notpick=f(i+1,r,st,m,dp);
+		return dp[i]=max(pick,notpick);
+	}
+
+	long long maxTaxiEarnings(int n,vector<vector<int>>& r) {
+		int m=r.size();
+		sort(r.begin(),r.end());
+		vector<int> st;
+		for(int i=0;i<m;i++) st.push_back(r[i][0]);
+		vector<long long> dp(m,-1);
+		return f(0,r,st,m,dp);
+	}
+};

77bf8124d8cafd507d7823c33502fe103b031134
Aditya Kumar <aditya220972@gmail.com>
2023-08-22 12:49:36 +0530

    Time: 12 ms (30.86%), Space: 15.2 MB (31.82%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-int fs(int i, vector<int>& prices, vector<vector<int>>& dp, bool buy) {
-   if(i==prices.size()) return 0;
-    if(dp[i][buy]!=-1) return dp[i][buy];
-   if(buy==true)
-   {
-    int sell=prices[i]+fs(i+1,prices,dp,false);
-    int notsell= fs(i+1,prices,dp,buy);
-    dp[i][buy]=max(sell,notsell);
-   }
-   else
-   {
-    int by=-prices[i]+fs(i+1,prices,dp,true);
-    int notby= fs(i+1,prices,dp,buy);
-    dp[i][buy]=max(by,notby);
-   }
-   return dp[i][buy];
-}
 int maxProfit(vector<int>& prices) {
-   vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
-    int b = fs(0, prices, dp, false);              // Not buying on the first day
-    return  b;
+    int n=prices.size();
+   vector<vector<int>>dp(n+1,vector<int>(2,0));
+    dp[n][0]=0;
+    dp[n][1]=0;    //not index lleft
+
+    for(int i=n-1;i>=0;i--)
+    {
+       for(int j=0;j<=1;j++)
+       {
+          if(j==1)
+          {
+            dp[i][j]=max(prices[i]+dp[i+1][j-1],dp[i+1][j]);
+          } 
+          if(j==0)
+          {
+            dp[i][j]=max(-prices[i]+dp[i+1][j+1],dp[i+1][j]);
+          } 
+       }
+    }
+    return dp[0][0];
+    // print2d(dp,n,2);
 }
 };

ee450ee79b3e30e23fe26c09a05bd76f06743dd5
Aditya Kumar <aditya220972@gmail.com>
2023-08-23 03:46:51 +0530

    Time: 191 ms (43.12%), Space: 94.8 MB (28.39%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-  int maxProfit(vector<int>& prices, int fee) {
-  int n=prices.size();
-       int obs=-prices[0];
-       int oss=0;
-       for(int i=0;i<n;i++)
-       {
-          int nbs=0;
-          int nss=0;
-          nbs=max(obs,oss-prices[i]);
-          nss=max(oss,obs+prices[i]-fee);   //pepcoding ki vdo nbs is new buy state 
-                                        //jo hai max of obs aur phle ka deal off and new opening 
-          // deb(i);
-          // deb(nbs);
-          // deb(nss);
-          obs=nbs;
-          oss=nss;
-       }
-     return oss;
+int fs(int i,vector<int>& prices,vector<vector<int>>&dp,int buy,int fee)
+{
+    if(i>=prices.size()) return 0;
+    if(dp[i][buy]!=-1) return dp[i][buy];
+    if(buy==1)
+    {
+        int sl=fs(i+1,prices,dp,0,fee)+prices[i]-fee;
+        int ns=fs(i+1,prices,dp,buy,fee);
+        dp[i][buy]=max(sl,ns);
     }
-
+    else if(buy==0)
+    {
+         int b=fs(i+1,prices,dp,1,fee)-prices[i];
+        int nb=fs(i+1,prices,dp,0,fee);
+        dp[i][buy]=max(b,nb);
+    }
+    return dp[i][buy];
+}
+    int maxProfit(vector<int>& prices,int fee) {
+        int n=prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        return fs(0,prices,dp,0, fee);
+    }
 };

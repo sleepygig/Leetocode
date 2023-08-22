a517dcc1e5927a3471e9b5beb8bea825f9d990f9
Aditya Kumar <aditya220972@gmail.com>
2023-08-22 12:14:54 +0530

    Time: 14 ms (23.3%), Space: 17.2 MB (12.49%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-            int sum=0;
-    for (int i=1; i<prices.size();i++)
-        {
-            if(prices[i-1]<prices[i])
-            {sum=sum+prices[i]-prices[i-1];}
-        }
-        return sum; 
-    }
+int fs(int i, vector<int>& prices, vector<vector<int>>& dp, bool buy) {
+   if(i==prices.size()) return 0;
+    if(dp[i][buy]!=-1) return dp[i][buy];
+   if(buy==true)
+   {
+    int sell=prices[i]+fs(i+1,prices,dp,false);
+    int notsell= fs(i+1,prices,dp,buy);
+    dp[i][buy]=max(sell,notsell);
+   }
+   else
+   {
+    int by=-prices[i]+fs(i+1,prices,dp,true);
+    int notby= fs(i+1,prices,dp,buy);
+    dp[i][buy]=max(by,notby);
+   }
+   return dp[i][buy];
+}
+int maxProfit(vector<int>& prices) {
+   vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
+    int b = fs(0, prices, dp, false);              // Not buying on the first day
+    return  b;
+}
 };

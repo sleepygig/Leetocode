9901a1dc2d547a009145ed5aa7c023287adb6bdc
Aditya Kumar <aditya220972@gmail.com>
2023-08-28 20:28:37 +0530

    Time: 7 ms (10.47%), Space: 7.8 MB (11.21%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,34 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <sstream>
+
 class Solution {
 public:
-int fs(unordered_map<int,char>&mp,string s,int i,int j,vector<vector<int>>&dp)
-    {
-        if (i==s.size() and j==s.size()) return 1;
-        if (i!=s.size() and j==s.size()) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        string t=s.substr(i,j-i+1);
-        int a;
-        stringstream(t) >> a;
-        if(mp.find(a)!=mp.end()  and t[0]!='0')
-        {
-            return dp[i][j]=fs(mp,s,j+1,j+1,dp)+fs(mp,s,i,j+1,dp);
+    int fs(unordered_map<int, char> &mp, string s, int j, vector<int> &dp) {
+        if (j == s.size()) return 1;
+        if (dp[j] != -1) return dp[j];
+        
+        int ways = 0;
+        for (int i = j; i < s.size(); ++i) {
+            string t = s.substr(j, i - j + 1);
+            int a;
+            stringstream(t) >> a;
+            
+            if (mp.find(a) != mp.end() && t[0] != '0') {
+                ways += fs(mp, s, i + 1, dp);
+            } else {
+                break;  // Stop considering further substrings if invalid
+            }
         }
-        else 
-        {
-
-        return dp[i][j]=fs(mp,s,i,j+1,dp);
-        }
-        return 0;
+        
+        dp[j] = ways;
+        return ways;
     }
-   int numDecodings(string s) {
-    unordered_map<int ,char>mp;
-    if(s[0]=='0') return 0;
-    for(char i='a';i<='z';i++)
-        {
-            mp[i-96]=i;
+
+    int numDecodings(string s) {
+        unordered_map<int, char> mp;
+        if (s[0] == '0') return 0;
+        for (char i = 'a'; i <= 'z'; i++) {
+            mp[i - 96] = i;
         }
-          vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1));
-        return fs(mp,s,0,0,dp);
-      
-     
+
+        vector<int> dp(s.size() + 1, -1);
+        return fs(mp, s, 0, dp);
     }
 };
